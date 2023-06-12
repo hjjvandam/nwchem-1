0b1bb971d79e2e8b0d87320bd8cc77cd3f98366c
hjjvandam <hvandam@bnl.gov>
2023-06-11 21:18:54 -0400

    Adding stretching runs.

---
+++ QA/tests/1fme/run_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+../../../bin/LINUX64/nwchem 1fme_prepare.nw     2>&1 | tee 1fme_prepare.out
+../../../bin/LINUX64/nwchem 1fme_minimize.nw    2>&1 | tee 1fme_minimize.out
+cp nwchemdat_md.qrs nwchemdat_md.rst
+#../../../bin/LINUX64/nwchem 1fme_equilibrate.nw 2>&1 | tee 1fme_equilibrate.out
+../../../bin/LINUX64/nwchem 1fme_dynamics.nw    2>&1 | tee 1fme_dynamics.out
+../../../bin/LINUX64/nwchem 1fme_analysis.nw    2>&1 | tee 1fme_analysis.out
+
